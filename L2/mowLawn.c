42af7a208420748094d9cbcf781a143b2e5cb3dc
sebxGit <72699667+sebxGit@users.noreply.github.com>
2020-11-12 13:36:40 +0100

    Update mowLawn.c

--- L2/mowLawn.c
+++ L2/mowLawn.c
@@ -3,7 +3,7 @@
 
 int main(){
     int ln_f, wd_f, ln_h, wd_h, area_f, area_h, area_g, a, ct;
-    
+
     printf("Type length and width of foundation:\n");
     scanf("%d", &ln_f);
     scanf("%d", &wd_f);
@@ -15,7 +15,7 @@ int main(){
     /*Finding area*/
     area_f = ln_f * wd_f;
     area_h = ln_h * wd_h;
-    area_g = area_f - area_h;    
+    area_g = area_f - area_h;
 
     printf("Foundation: %d, House: %d, Grass: %d", area_f, area_h, area_g);
 
