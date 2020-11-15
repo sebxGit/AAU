48e14cd52c2e0f880ceddd424313ca3184d1fc50
sebxGit <72699667+sebxGit@users.noreply.github.com>
2020-11-16 00:08:47 +0100

    Added yatzy

--- Eksamensopgaver/Yatzy.c
+++ Eksamensopgaver/Yatzy.c
@@ -6,9 +6,73 @@
 /* Sebastian Truong, Grp. A409, Aalborg Universitet Software 1. semester 2020 */
 
 /* This program plays yatzy with itself. Takes the 5 highest numbered rolled die
-   and then shows all the rounds of the program.*/
+   and then shows all the rounds of the program.
+   My program does not implement the rule: At least 3 alike dice rolls to add points.*/
+/* ER DER 3 ENS, HVIS DER ER 4? Ændr på KINDS, FULL HOUSE*/
 
-int promt(){
+int prompt();
+int n_cmp(const void *ep1, const void *ep2);
+int *roll_multiple_dies(int ct);
+int alike(const int tb[], const int dies_total, int *score, int i);
+int pair(const int tb[], const int dies_total, int *score);
+int ofAKind(const int tb[], const int dies_total, int *score);
+void straight(const int tb[], const int dies_total, int *score);
+void fullHouse(const int tb[], int *score, int pairs, int kinds);
+void chance(const int tb[], const int dies_total, int *score);
+void yatzy(const int tb[], const int dies_total, int *score);
+void final_score(int score, int bonus_points);
+int try_again();
+
+void get_score(const int tb[], const int dies_total, int *score, int round, int *bonus_points){
+  int pairs, kinds, straights, fullHouses, chances, yatzies;
+
+  if (round == 1){              /* Upper section with 1-6. */
+    for (int i = 1; i <= 6; i++){
+      if (i != 1){
+        int *tb = roll_multiple_dies(dies_total);
+      }
+      printf("Number %d: Amount: %d, Score: %d\n", i, alike(tb, dies_total, &*score, i), *score); 
+    }
+    if (*score >= 63)
+      *bonus_points = 50;
+  }  /* Lower section. Returns the commented values.*/ 
+  else if (round == 2){      
+      pairs = pair(tb, dies_total, &*score); /* returns highest pair (to fullHouses). */
+  } else if (round == 3){
+      kinds = ofAKind(tb, dies_total, &*score); /* returns three kind (to fullHouses) */
+  } else if (round == 4){
+      straight(tb, dies_total, &*score); 
+  } else if (round == 5){
+      fullHouse(tb, &*score, pairs, kinds); 
+  } else if (round == 6){
+      chance(tb, dies_total, &*score);
+  } else if (round == 7){
+      yatzy(tb, dies_total, &*score);
+  }  
+  printf("\n");
+}
+
+int main(void){
+  time_t t;
+  srand((unsigned)time(&t));
+  int score = 0, bonus_points = 0;
+  int running = 1;
+
+  while (running){
+    int dies_total = prompt(); /* Returns n count of wanted die rolled. */
+
+    for (int rounds = 1; rounds <= 8; rounds++){
+      int *tb = roll_multiple_dies(dies_total);
+      get_score(tb, dies_total, &score, rounds, &bonus_points);
+      free(tb);
+    }
+    final_score(score, bonus_points);
+    running = try_again();
+    }
+  return 0;    
+}
+
+int prompt(){
   int dies_called = 0;
 
   do{
@@ -29,71 +93,6 @@ int n_cmp(const void *ep1, const void *ep2){
     return 0;
 }
 
-/*
-int *check_combinations(int tb[], const int dies_total, int ct, char message){
-  int new_tb[dies_total];
-  if (message == "max"){
-    for (int i = 0; i < dies_total; i++)
-      new_tb[i] = tb[ct - 5 + i];
-  }
-
-  return;
-} */
-
-int *roll_multiple_dies(int ct);
-int alike(const int tb[], const int dies_total, int *score, int i);
-int pair(const int tb[], const int dies_total, int *score);
-int ofAKind(const int tb[], const int dies_total, int *score);
-int straight(const int tb[], const int dies_total, int *score);
-int fullHouse(const int tb[], int *score, int pairs, int kinds);
-int chance(const int tb[], const int dies_total, int *score);
-int yatzy(const int tb[], const int dies_total, int *score);
-
-void get_score(const int tb[], const int dies_total, int *score, int round){
-  int pairs, kinds, straights, fullHouses, chances, yatzies;
-
-  if (round == 1){              /* Upper section with 1-6. */
-    for (int i = 1; i <= 6; i++){
-      printf("Number %d: Amount: %d, Score: %d\n", i, alike(tb, dies_total, &*score, i), *score); 
-      int *tb = roll_multiple_dies(dies_total);
-    }
-  } else if (round == 2){       /* Lower section. Returns the commented values.*/ 
-      pairs = pair(tb, dies_total, &*score); /* Number of pairs. */
-  } else if (round == 3){
-      kinds = ofAKind(tb, dies_total, &*score); /* Four kinds: 4, Three kinds: 3 and None: 0. */
-  } else if (round == 4){
-      straights = straight(tb, dies_total, &*score); /* Small: -1, Large: 1 and None: 0. */
-  } else if (round == 5){
-      fullHouses = fullHouse(tb, &*score, pairs, kinds); /* Full house: 1, None: 0. */
-  } else if (round == 6){
-      chances = chance(tb, dies_total, &*score); /* Completed: 1 */
-  } else if (round == 7){
-      yatzies = yatzy(tb, dies_total, &*score); /* Yatzy: 1, None 0. */
-  }
-}
-
-int main(void){
-  time_t t;
-  srand((unsigned)time(&t));
-
-  int dies_total = promt(); /* Returns n count of wanted die rolled. */
-  int score = 0;
-  for (int rounds = 1; rounds <= 8; rounds++){
-    int *tb = roll_multiple_dies(dies_total);
-    printf("\n");
-    for (int k = 0; k < dies_total; k++)
-      printf("%d  ", tb[k]);
-    printf("\n");
-    get_score(tb, dies_total, &score, rounds);
-    free(tb);
-  }
-  printf("Final Score: %d\n", score);
-    /*
-  draw_scoreboard();
-  play_again();*/
-  return 0;
-}
-
 int *roll_multiple_dies(const int dies_total){
   int *tb = (int *)malloc(dies_total * sizeof(int));
   if (tb == NULL){
@@ -105,6 +104,10 @@ int *roll_multiple_dies(const int dies_total){
 
   /* Sorts the array with the qsort algorithm in an ascending order. */
   qsort(tb, dies_total, sizeof(int), n_cmp);
+
+  /* Prints the numbers.*/
+  for (int k = 0; k < dies_total; k++)
+    printf("%d  ", tb[k]);
   return tb;
 }
 
@@ -122,9 +125,11 @@ int alike(const int tb[], const int dies_total, int *score, int i){
 }
 
 int pair(const int tb[], const int dies_total, int *score){
-  int prev_num = -1, pairs = 0;
+  int prev_num = -1, pairs = 0, highest_pair = 0;
     for (int i = dies_total; i > 0; i--){
       if (prev_num == tb[i - 1]){
+        if (!highest_pair)
+          highest_pair = prev_num;
         pairs += 1;
         *score += tb[i] + prev_num;
         i--;
@@ -134,8 +139,8 @@ int pair(const int tb[], const int dies_total, int *score){
     else 
       prev_num = tb[i - 1];
     } 
-  printf("\nPairs: %d, Score: %d", pairs, *score);
-  return pairs;
+  printf("Pairs: %d, Score: %d", pairs, *score);
+  return highest_pair;
 }
 
 int ofAKind(const int tb[], const int dies_total, int *score){
@@ -143,75 +148,100 @@ int ofAKind(const int tb[], const int dies_total, int *score){
   for (int i = dies_total; i > 0; i--){
     if (tb[i] == tb[i - 1] && tb[i] == tb[i - 2] && tb[i] == tb[i - 3]){
       *score += tb[i] * 4;
-      printf("\nFour of a kind: Num %d, Score: %d", tb[i], *score);
-      return 4;
+      printf("Four of a kind: Num %d, Score: %d", tb[i], *score);
+      return 0;
     }
   }
   /* Then proceedes with the three alike. */
-  for (int i = 0; i < dies_total - 2; i++){
-    if (tb[i] == tb[i + 1] && tb[i] == tb[i + 2]){
+  for (int i = dies_total; i > 0; i--){
+    if (tb[i] == tb[i-1] && tb[i] == tb[i - 2]){
       *score += tb[i] * 3;
-      printf("\nThree of a kind: Num %d, Score: %d", tb[i], *score);
-      return 3;
+      printf("Three of a kind: Num %d, Score: %d", tb[i], *score);
+      return tb[i];
     }
   } 
-  printf("\nNone of a kind: Score: %d", *score);
+  printf("None of a kind: Score: %d", *score);
   return 0;
 }
 
-int straight(const int tb[], const int dies_total, int *score){
+void straight(const int tb[], const int dies_total, int *score){
+
   /* Making a new string from the original array to a string. */
-  char tb_inString[6];
+  char tb_inString[dies_total + 1];
   for (int i = 0; i < dies_total; i++)
     tb_inString[i] = tb[i] + '0'; /* Syntax for converting an int to a char. */
   
-  tb_inString[5] = '\0'; /* Strings have this null value on the last element of the array.*/
+  tb_inString[dies_total] = '\0'; /* Strings have this null value on the last element of the array. */
   
-  if (strcmp(tb_inString, "12345") == 0){
-    *score += 15;
-    printf("\nSmall Straight! Score: %d", *score);
-    return -1;
+  /* Creates another string without dublicates. */
+  int prev_num = -1, n = 0;
+  char tb_sorted[7];
+  for (int i = 0; i < dies_total; i++){
+    if (tb_inString[i] == prev_num)
+      continue;
+    tb_sorted[n] = tb_inString[i];
+    prev_num = tb_inString[i];
+    n++;
   }
+  tb_sorted[6] = '\0';
 
-  if (strcmp(tb_inString, "23456") == 0){
+  if (strcmp(tb_sorted, "23456") == 0 || strcmp(tb_sorted, "123456") == 0){ /* Strcmp(a, b) returns 0 if identical. */
     *score += 20;
-    printf("\nLarge Straight! Score: %d", *score);
-    return 1;
-  } 
-  printf("\nNo Straights. Score: %d", *score);
-  return 0;
-}
-
-int fullHouse(const int tb[], int *score, int pairs, int kinds){
-  if (pairs == 1 && kinds == 3){
-    *score += (tb[0] == tb[2]) ? tb[0] * 3 + tb[4] * 2 : tb[0] * 2 + tb[4] * 3;
-    printf("\nFull House! Score %d", *score);
-    return 1;
-  } 
-  printf("\nNo Full House. Score: %d", *score);
-  return 0;
-}
-
-int chance(const int tb[], const int dies_total, int *score){
-  for (int i = 0; i < dies_total; i++)
-    *score += tb[i];
-  printf("\nChance! Score: %d", *score);
-  return 1;
-}
-
-int yatzy(const int tb[], const int dies_total, int *score){
-  /* Making a new string from the original array to a string. */
-  char tb_inString[6];
-  for (int i = 0; i < dies_total; i++)
-    tb_inString[i] = tb[i] + '0'; /* Syntax for converting an int to a char. */
-  
-  tb_inString[5] = '\0'; /* Strings have this null value on the last element of the array.*/
-  
-  if (strcmp(tb_inString, "55555") == 0){
-        *score += 15;
-        printf("\nYatzy! Score: %d", *score);
-        return 1;
+    printf("Large Straight! Score: %d", *score);
+  } else if (strcmp(tb_sorted, "12345") == 0){
+    *score += 15;
+    printf("Small Straight! Score: %d", *score);
+  }else{
+    printf("No Straights. Score: %d", *score);
   }
-  printf("\nNo Yatzy. Score: %d", *score);
-  return 0;
+}
+
+void fullHouse(const int tb[], int *score, int pairs, int kinds){
+  if (pairs > 0 && kinds > 0){
+    *score += pairs * 2 + kinds * 3;
+    printf("Full House! Score %d", *score);
+  } else{
+    printf("No Full House. Score: %d", *score);
+  }
+}
+
+void chance(const int tb[], const int dies_total, int *score){
+  for (int i = 0; i < dies_total; i++){
+    *score += tb[dies_total - i];
+    if (i == 5)
+      break;
+  }
+  printf("Chance! Score: %d", *score);
+}
+
+void yatzy(const int tb[], const int dies_total, int *score){
+  for (int i = dies_total; i > 0; i--){
+    if (tb[i] == tb[i - 1] && tb[i] == tb[i - 2] && tb[i] == tb[i - 3] && tb[i] == tb[i - 4]){
+      *score += tb[i] * 5;
+      printf("Yatzy! Num %d, Score: %d", tb[i], *score);
+      break;
+    } else if (i == 1){
+      printf("No Yatzy. Score: %d", *score);
+    }
+  }
+}
+
+void final_score(int score, int bonus_points){
+  score += bonus_points;
+  printf("Final Score: %d\n", score);
+  printf("Bonus points awarded: %d\n", bonus_points);
+}
+
+int try_again(){
+  char ans[1];
+  do{
+  printf("Do you wish to try again? (y/n) ");
+  scanf("%s", ans);
+  if (strcmp(ans, "y") == 0)
+    printf("HEY!");
+  } while (!(strcmp(ans, "y") == 0 || strcmp(ans, "n") == 0));
+  if (strcmp(ans, "y") == 0)
+    return 1;
+  else
+    return 0;
 }
