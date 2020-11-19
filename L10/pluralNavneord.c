e6265e21aebb7addb98ec62605936b8567a6e6fc
SebxGit <minhseb@gmail.com>
2020-11-19 11:56:29 +0100

    a

--- L10/pluralNavneord.c
+++ L10/pluralNavneord.c
@@ -5,18 +5,19 @@
 #define WORDS 9
 
 int main(void){
-  char words[9] = {"chair", "dairy", "boss", "circus", "fly", "dog", "church", "clue", "dish"};
+  char words[WORDS][8] = {"chair", "dairy", "boss", "circus", "fly", "dog", "church", "clue", "dish"};
   char *tb = (char *)malloc(WORDS * sizeof(char));
   if (tb == NULL){
     printf("Cannot allocate memory. Exit.");
     exit(EXIT_FAILURE);
   }
-  for (int w = 0; w < WORDS; w++){
+
+  for (int w = 0; w < WORDS; w++)
     tb[w] = words[w];
-  }
 
-  for (int i = 0; i < len(tb[i]); i++){
-    printf("tb[%d] = %c ", i, tb[i]);
-  }
-
+  printf("\n");
+  for (int i = 0; i < WORDS; i++)
+    printf("%s ", words[i]);
+  free(tb);
+  return 0;
 }
