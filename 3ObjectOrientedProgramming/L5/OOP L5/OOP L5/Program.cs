27dda8e2c18a2153c06eb452368a5d2919a3ba60
SebxGit <minhseb@gmail.com>
2021-10-22 08:57:27 +0200

    ff

---
+++ 3ObjectOrientedProgramming/L5/OOP L5/OOP L5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OOP_L5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*opg1
+            sm s = new();
+
+            try
+            {
+                var a = s.ReadInteger();
+            }
+            catch
+            {
+                throw new System.FormatException();
+            }*/
+            
+        }
+    }
+
+    //opg1
+    //class sm
+    //{
+    //    public int ReadInteger()
+    //    {
+    //        Console.WriteLine("Insert input");
+    //        var input = Console.ReadLine();
+    //        return int.Parse(input);
+    //    }
+    //}
+
+    //opg2
+    class BankAccount
+    {
+
+    }
+}
