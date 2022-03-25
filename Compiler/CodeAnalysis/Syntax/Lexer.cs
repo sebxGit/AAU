90d2e5e4035028d1573857c9751a5fddd1feb1ff
SebxGit <minhseb@gmail.com>
2022-03-25 14:23:55 +0100

    temp

--- Compiler/CodeAnalysis/Syntax/Lexer.cs
+++ Compiler/CodeAnalysis/Syntax/Lexer.cs
@@ -16,14 +16,15 @@ namespace Compiler.CodeAnalysis.Syntax
 
         public IEnumerable<string> Diagnostics => _diagnostics;
 
-        private char Current
-        {
-            get
-            {
-                if (_position >= _text.Length) return '\0';
+        private char Current = > Peek(0);
+        private char Lookahead = > Peek(1);
 
-                return _text[_position];
-            }
+        private char Peek(int offset)
+        {
+            var index = _position + offset;
+            if (index >= _text.Length) return '\0';
+
+            return _text[_position];
         }
 
         private void Next()
@@ -83,6 +84,15 @@ namespace Compiler.CodeAnalysis.Syntax
                 case '/': return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
                 case '(': return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
                 case ')': return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+                case '!': return new SyntaxToken(SyntaxKind.BangToken, _position++, "!", null);
+                case '&': 
+                    if (Lookahead == '&')
+                        return new SyntaxToken(SyntaxKind.AmpersandAmpersandToken, _position += 2, "&&", null);
+                    break;
+                case '|':
+                    if (Lookahead == '|')
+                        return new SyntaxToken(SyntaxKind.PipePipeToken, _position += 2, "||", null);
+                    break;
                 default:
                     _diagnostics.Add($"ERROR: bad character input: '{Current}'");
                     return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
